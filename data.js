5303fe63ea1f14a5527b98b1cf9161c7c0297e5f
SanihaManjunath <121946623+SanihaManjunath@users.noreply.github.com>
2026-01-31 16:46:55 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 const ATTACK_TYPES = [
   { name: "DDoS", color: "#00e5ff" },
   { name: "Phishing", color: "#ffd54f" },
-  { name: "Malware", color: "#9254de" },
-  { name: "Bruteforce", color: "#36cfc9" },
-  { name: "Ransomware", color: "#52c41a" }
+  { name: "Malware", color: "#b388ff" },
+  { name: "Bruteforce", color: "#1de9b6" },
+  { name: "Ransomware", color: "#69f0ae" }
 ];
 
 const LOCATIONS = [
