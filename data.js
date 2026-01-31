19b4fa3d4ce6d0449e54f5d664e347031cf2ca91
SanihaManjunath <121946623+SanihaManjunath@users.noreply.github.com>
2026-01-31 10:58:14 +0530

    Update data.js

--- data.js
+++ data.js
@@ -8,14 +8,14 @@ const ATTACK_TYPES = [
 
 const LOCATIONS = [
   { country: "USA", lat: 37.7749, lng: -122.4194 },
+  { country: "UK", lat: 51.5074, lng: -0.1278 },
   { country: "Germany", lat: 52.52, lng: 13.405 },
   { country: "India", lat: 28.6139, lng: 77.209 },
   { country: "China", lat: 39.9042, lng: 116.4074 },
+  { country: "Japan", lat: 35.6895, lng: 139.6917 },
   { country: "Russia", lat: 55.7558, lng: 37.6173 },
-  { country: "Brazil", lat: -23.5505, lng: -46.6333 },
-  { country: "UK", lat: 51.5074, lng: -0.1278 },
-  { country: "Japan", lat: 35.6895, lng: 139.6917 }
+  { country: "Brazil", lat: -23.5505, lng: -46.6333 }
 ];
 
 let liveAttacks = [];
-let impactPoints = [];
+let regionHeat = [];
